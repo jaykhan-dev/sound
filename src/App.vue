2ed635f3f1a1fd5a27fc8e85875c233f7dc997cd
Jay Khan <jaykhan.sound@gmail.com>
2022-06-08 14:19:00 -0400

    sound check

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SideNav from "./components/SideNav.vue";
+</script>
+
+<template>
+  <header>
+    <SideNav />
+  </header>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main class="grid place-items-center h-screen">
+    <h1 class="lg:text-9xl font-bold">SOUND</h1>
+  </main>
+</template>
